ffd279ed434dd3e4c2325e814370f8250ac81be1
Judywen Guapin <37282871+jagwarthegreat@users.noreply.github.com>
2022-02-17 22:50:03 +0800

    Update article.view.php

--- app/views/article.view.php
+++ app/views/article.view.php
@@ -21,7 +21,7 @@ if (!empty($blog['users'][0])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta property="og:title" content="<?= $blog['title'] ?>" />
-    <meta property="og:description" content="<?= (!empty($blog['content'])) ? $pd->text(html_entity_decode(html_entity_decode($blog['content']))) : ''; ?>" />
+    <meta property="og:description" content="click here to read the full blog" />
     <!-- <meta property="og:image" content="" /> -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='icon' href='<?= public_url('/favicon.ico') ?>' type='image/ico' />
